b8f6f844af3f2acc1d09596ae5a491fe7aad3c98
SebastianZasadni <112890476+SebastianZasadni@users.noreply.github.com>
2023-07-15 08:44:35 +0000

    Deploying to gh-pages from @ SebastianZasadni/goit-react-hw-08-phonebook@7ad238d83b9b3ca956656ea283798ed17626f13d 🚀

---
+++ static/js/936.dc4730e0.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[936],{4936:function(e,n,r){r.r(n),r.d(n,{default:function(){return o}});var t="Home_container__w+u63",a="Home_title__L087v",c=r(3329),o=function(){return(0,c.jsx)("div",{className:t,children:(0,c.jsxs)("h1",{className:a,children:["Phonebook"," ",(0,c.jsx)("span",{role:"img","aria-label":"Greeting icon",children:"\ud83d\udc81\u200d\u2640\ufe0f"})]})})}}}]);
+//# sourceMappingURL=936.dc4730e0.chunk.js.map
