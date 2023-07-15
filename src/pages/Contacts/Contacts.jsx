049e9adf3a92c2a2ddf03ded1dae74fcb37019fe
SebastianZasadni <emblelele@gmail.com>
2023-07-15 10:33:50 +0200

    fix page contacts

--- src/pages/Contacts/Contacts.jsx
+++ src/pages/Contacts/Contacts.jsx
@@ -2,12 +2,13 @@ import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Helmet } from 'react-helmet';
 import { ContactsList } from 'components/ContactsList/ContactsList';
-import { fetchContacts } from 'redux/tasks/operations';
-import { selectLoading } from 'redux/tasks/selectors';
+import { ContactsForm } from 'components/ContactsForm/ContactsForm';
+import { fetchContacts } from 'redux/contacts/operations';
+import { selectIsLoading } from 'redux/contacts/selectors';
 
 export default function Contacts() {
   const dispatch = useDispatch();
-  const isLoading = useSelector(selectLoading);
+  const isLoading = useSelector(selectIsLoading);
 
   useEffect(() => {
     dispatch(fetchContacts());
@@ -18,7 +19,7 @@ export default function Contacts() {
       <Helmet>
         <title>Your contacts</title>
       </Helmet>
-      <TaskEditor />
+      <ContactsForm />
       <div>{isLoading && 'Request in progress...'}</div>
       <ContactsList />
     </>
