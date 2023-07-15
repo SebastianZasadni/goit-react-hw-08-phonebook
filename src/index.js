c7f9c40dd4fd135a72b7b132ecd374f787cfebcb
SebastianZasadni <emblelele@gmail.com>
2023-07-15 10:10:41 +0200

    feature redux-persist

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {Brow}
+import { BrowserRouter } from 'react-router-dom';
 import { store } from 'redux/store';
 import { Provider } from 'react-redux';
 import { App } from 'App';
