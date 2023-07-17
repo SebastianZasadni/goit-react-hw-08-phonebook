f3f5ae844764d73ec4e1c45d81b62f0fd9096420
SebastianZasadni <emblelele@gmail.com>
2023-07-17 17:26:22 +0200

    delete helmet

--- src/pages/Contacts/Contacts.jsx
+++ src/pages/Contacts/Contacts.jsx
@@ -1,6 +1,5 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Helmet } from 'react-helmet';
 import { ContactsList } from 'components/ContactsList/ContactsList';
 import { ContactsForm } from 'components/ContactsForm/ContactsForm';
 import { fetchContacts } from 'redux/contacts/operations';
@@ -16,9 +15,7 @@ const Contacts = () => {
 
   return (
     <>
-      <Helmet>
-        <title>Your contacts</title>
-      </Helmet>
+      <title>Your contacts</title>
       <ContactsForm />
       <div>{isLoading && 'Request in progress...'}</div>
       <ContactsList />
